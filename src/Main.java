75c726e4944f93704f4c58307512b1418c3c78cf
Artem <Azumishinji.jp@gmail.com>
2023-02-03 00:47:29 +0300

    add: calculation of Roman numbers

--- src/Main.java
+++ src/Main.java
@@ -23,23 +23,22 @@ public class Main {
     /**
      * Выполняет указанную операцию над двумя операндами
      *
-     * @param a первый операнд
-     * @param b второй операнд
+     * @param a        первый операнд
+     * @param b        второй операнд
      * @param operator операция
      * @return String Результат математической операции
      */
     private static String calc(String a, String b, String operator) {
         Pattern arabictPattern = Pattern.compile("-?\\d+");
-        Pattern romanianPattern = Pattern.compile("-?\\w+");
+        Pattern romanPattern = Pattern.compile("-?\\w+");
         String result = "";
         try {
             if (arabictPattern.matcher(a).matches() &&
                     arabictPattern.matcher(b).matches()) {
                 result = String.valueOf(calcArabian(Integer.parseInt(a), Integer.parseInt(b), operator));
-            } else if (romanianPattern.matcher(a).matches() &&
-                    romanianPattern.matcher(b).matches()) {
-//                calcRomanian(a, b, operator);
-                System.out.println("under construction");
+            } else if (romanPattern.matcher(a).matches() &&
+                    romanPattern.matcher(b).matches()) {
+                result = calcRoman(a, b, operator);
             } else {
                 throw new Exception("Используются одновременно разные системы счисления!");
             }
@@ -52,8 +51,8 @@ public class Main {
     /**
      * Выполняет указанную операцию над двумя арабскими числами
      *
-     * @param a первый операнд
-     * @param b второй операнд
+     * @param a        первый операнд
+     * @param b        второй операнд
      * @param operator операция
      * @return String Результат математической операции
      */
@@ -68,5 +67,101 @@ public class Main {
         };
     }
 
-//    private
+    /**
+     * Выполняет указанную операцию над двумя римскими числами
+     *
+     * @param a        первый операнд
+     * @param b        второй операнд
+     * @param operator операция
+     * @return String Результат математической операции
+     */
+    private static String calcRoman(String a, String b, String operator) {
+        int arabianA = convertRomToArab(a);
+        int arabianB = convertRomToArab(b);
+        int result = calcArabian(arabianA, arabianB, operator);
+
+        return convertArabToRom(result);
+    }
+
+    /**
+     * Конверрует римское число в арабское
+     *
+     * @param number Строка содержащая римское число
+     * @return Целое арабское число
+     */
+    private static int convertRomToArab(String number) {
+        if (number.equalsIgnoreCase("IV")) {
+            return 4;
+        } else if (number.equalsIgnoreCase("IX")) {
+            return 9;
+        }
+        char[] charNumber = number.toUpperCase().toCharArray();
+        int result = 0;
+        for (char currentChar :
+                charNumber) {
+            switch (currentChar) {
+                case ('I'):
+                    result += 1;
+                    break;
+                case ('V'):
+                    result += 5;
+                    break;
+                case ('X'):
+                    result += 10;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Конвертирует арабское число в римское
+     *
+     * @param number Целое число
+     * @return Строку с римским числом
+     */
+    private static String convertArabToRom(int number) {
+        String romanNumber = "";
+        int units = 0;
+        int tens = 0;
+        int hundreds = 0;
+        try {
+            if (number <= 0) {
+                throw new Exception("В римской системе нет отрицательных чисел и нуля!");
+            } else if (number < 10) {
+                units = number;
+            } else if (number < 100) {
+                units = number % 10;
+                tens = number / 10;
+            } else {
+                return "C";
+            }
+
+            if (tens != 0) {
+                if (tens < 4) {
+                    romanNumber += "X".repeat(tens);
+                } else if (tens == 4) {
+                    romanNumber += "XL";
+                } else if (tens < 9) {
+                    romanNumber += "L" + "X".repeat(tens - 5);
+                } else {
+                    romanNumber += "XC";
+                }
+            }
+
+            if (units < 4) {
+                romanNumber += "I".repeat(units);
+            } else if (units == 4) {
+                romanNumber += "IV";
+            } else if (units < 9) {
+                romanNumber += "V" + "I".repeat(units - 5);
+            } else {
+                romanNumber += "IX";
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return romanNumber;
+    }
 }
